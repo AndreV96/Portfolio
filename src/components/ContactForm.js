6218d1a7a4a9f5f1985428d3a22e99e63552eaa9
André Vega <ventaslederer@hotmail.com>
2022-04-21 15:50:21 -0500

    Added the contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,16 +1,30 @@
 import React from "react";
+import {Form, Button, Container, InputGroup, FormControl} from 'react-bootstrap';
 
 function ContactForm() {
   return (
     <div className="App">
-      <h2>Contact</h2>
-      <label>Name:</label>
-      <input></input>
-      <label>Email Address:</label>
-      <input></input>
-      <label>Message:</label>
-      <textarea></textarea>
-      <button>Submit</button>
+      <Container className="py-3 container-form">
+        <h1>Contact</h1>
+        <Form className="text-start">
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="name" placeholder="Enter name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicMessage">
+            <Form.Label>Message</Form.Label>
+            <FormControl as="textarea" aria-label="With textarea" placeholder="Enter a message"/>
+          </Form.Group>
+          {/* Add a type="submit" to add the functionality */}
+          <Button variant="secondary">
+            Submit
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 }
