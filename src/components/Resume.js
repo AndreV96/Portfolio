8ae7c1260c0a3e544d8443da34bc96c9fa54b6d7
André Vega <ventaslederer@hotmail.com>
2022-04-20 23:41:01 -0500

    Added cards and list in resume component.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,43 @@
 import React from "react";
+import {Card, ListGroup, ListGroupItem, Container} from 'react-bootstrap';
 
 function Resume() {
   return (
     <div className="App">
-      hey
+      <Container className="text-start">
+        <h2 >Resume</h2>
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>Front-end Proficiencies</Card.Title>
+          </Card.Body>
+          <ListGroup>
+            <ListGroupItem>HTML5</ListGroupItem>
+            <ListGroupItem>CSS3</ListGroupItem>
+            <ListGroupItem>Javascript</ListGroupItem>
+            <ListGroupItem>JQuery</ListGroupItem>
+            <ListGroupItem>Responsive Design</ListGroupItem>
+            <ListGroupItem>React</ListGroupItem>
+            <ListGroupItem>Bootstrap</ListGroupItem>
+            <ListGroupItem>Tailwind CSS</ListGroupItem>
+            <ListGroupItem>SASS</ListGroupItem>
+          </ListGroup>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>Back-end Proficiencies</Card.Title>
+          </Card.Body>
+          <ListGroup>
+            <ListGroupItem>APIs</ListGroupItem>
+            <ListGroupItem>Node</ListGroupItem>
+            <ListGroupItem>Express</ListGroupItem>
+            <ListGroupItem>MySQL, Sequelize</ListGroupItem>
+            <ListGroupItem>NoSQL</ListGroupItem>
+            <ListGroupItem>MongoDB, Mongoose</ListGroupItem>
+            <ListGroupItem>REST</ListGroupItem>
+            <ListGroupItem>GraphQL</ListGroupItem>
+          </ListGroup>
+        </Card>
+      </Container>
     </div>
   );
 }
