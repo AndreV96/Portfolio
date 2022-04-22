6e265c1e0634550ef8271e747ed082c63dca6dd7
André Vega <ventaslederer@hotmail.com>
2022-04-22 11:53:59 -0500

    Added card for the projects page and added image to de about me section

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,61 +7,61 @@ function Project() {
   return (
     <div className="App">
     <h1 className="mt-3">Projects</h1>
-      <Container className="my-3 projects-container">
-        <Card className="bg-dark text-white d-block project-card">
+      <Container className="my-3 projects-container align-items-center justify-content-center">
+        <a href="https://limitless-plateau-15055.herokuapp.com/" target="_blank" className="whistle-card"><Card className="bg-dark text-white d-block project-card">
           <Card.Img src="Whistle2.png" alt="Card image" className="project-img"/>
           <Card.ImgOverlay className="text-start">
             <Card.Title className="d-inline-block">Whistle</Card.Title>
-            <a href="https://github.com/AndreV96" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block"/></a> 
+            <a href="https://github.com/AndreV96/Whistle" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block projects-github-icon"/></a> 
             <Card.Text>
               HTML/CSS/JavaScript/NodeJs/Express/MySql
             </Card.Text>
           </Card.ImgOverlay>
-        </Card>
+        </Card></a>
         
-        <Card className="bg-dark text-white d-block project-card">
-          <Card.Img src="Whistle2.png" alt="Card image" className="project-img"/>
+        <a href="https://andrev96.github.io/Moviez/" target="_blank"><Card className="bg-dark text-white d-block project-card">
+          <Card.Img src="moviez_individual.png" alt="Card image" className="project-img"/>
           <Card.ImgOverlay className="text-start">
-            <Card.Title className="d-inline-block">Whistle</Card.Title>
-            <a href="https://github.com/AndreV96" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block"/></a> 
+            <Card.Title className="d-inline-block">Moviez</Card.Title>
+            <a href="https://github.com/AndreV96/Moviez/" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block projects-github-icon"/></a> 
             <Card.Text>
-              HTML/CSS/JavaScript/NodeJs/Express/MySql
+              HTML/CSS/JavaScript/Use of APIs
             </Card.Text>
           </Card.ImgOverlay>
-        </Card>
+        </Card></a>
 
-        <Card className="bg-dark text-white d-block project-card">
-          <Card.Img src="Whistle2.png" alt="Card image" className="project-img"/>
+        <a href="https://morning-lowlands-78728.herokuapp.com/" target="_blank"><Card className="bg-dark text-white d-block project-card">
+          <Card.Img src="Note_Taker.jpg" alt="Card image" className="project-img"/>
           <Card.ImgOverlay className="text-start">
-            <Card.Title className="d-inline-block">Whistle</Card.Title>
-            <a href="https://github.com/AndreV96" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block"/></a> 
+            <Card.Title className="d-inline-block">Simple Note</Card.Title>
+            <a href="https://github.com/AndreV96/Simple_Note" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block projects-github-icon"/></a> 
             <Card.Text>
-              HTML/CSS/JavaScript/NodeJs/Express/MySql
+              HTML/CSS/JavaScript/NodeJs/Express
             </Card.Text>
           </Card.ImgOverlay>
-        </Card>
+        </Card></a>
 
-        <Card className="bg-dark text-white d-block project-card">
-          <Card.Img src="Whistle2.png" alt="Card image" className="project-img"/>
+        <a href="https://andrev96.github.io/Forecasting/" target="_blank"><Card className="bg-dark text-white d-block project-card">
+          <Card.Img src="weather_dashboard.png" alt="Card image" className="project-img"/>
           <Card.ImgOverlay className="text-start">
-            <Card.Title className="d-inline-block">Whistle</Card.Title>
-            <a href="https://github.com/AndreV96" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block"/></a> 
+            <Card.Title className="d-inline-block">Forecasting</Card.Title>
+            <a href="https://github.com/AndreV96/Forecasting/" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block projects-github-icon"/></a> 
             <Card.Text>
-              HTML/CSS/JavaScript/NodeJs/Express/MySql
+              HTML/CSS/JavaScript/Use of APIs
             </Card.Text>
           </Card.ImgOverlay>
-        </Card>
+        </Card></a>
 
-        <Card className="bg-dark text-white d-block project-card">
-          <Card.Img src="Whistle2.png" alt="Card image" className="project-img"/>
+        <a href="https://andrev96.github.io/Every_Day/" target="_blank" className="ever-day-card"><Card className="bg-dark text-white d-block project-card ">
+          <Card.Img src="work_day.jpg" alt="Card image" className="project-img every-day-img"/>
           <Card.ImgOverlay className="text-start">
-            <Card.Title className="d-inline-block">Whistle</Card.Title>
-            <a href="https://github.com/AndreV96" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block"/></a> 
+            <Card.Title className="d-inline-block">Every Day</Card.Title>
+            <a href="https://github.com/AndreV96/Every_Day/" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block projects-github-icon"/></a> 
             <Card.Text>
-              HTML/CSS/JavaScript/NodeJs/Express/MySql
+              HTML/CSS/JavaScript
             </Card.Text>
           </Card.ImgOverlay>
-        </Card>
+        </Card></a>
       </Container>
     </div>
   );
