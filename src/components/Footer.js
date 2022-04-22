3d5b0655ef64330a400e0736186eecb79c4dd4f6
André Vega <ventaslederer@hotmail.com>
2022-04-22 12:33:06 -0500

    Added whatsapp and email links to the footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 import React from "react";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
+import {faLinkedin, faGithub, faWhatsapp} from '@fortawesome/free-brands-svg-icons'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
 
 function Footer() {
   return (
     <div className="App py-3 bg-dark bg-gradient">
       <a href="https://github.com/AndreV96" target="_blank"><FontAwesomeIcon icon={faGithub}  className="me-3 footer-icons"/></a> 
-      <a href="https://www.linkedin.com/in/andre-vega-lederer/" target="_blank"><FontAwesomeIcon icon={faLinkedin} className=" footer-icons"/></a>
+      <a href="https://www.linkedin.com/in/andre-vega-lederer/" target="_blank"><FontAwesomeIcon icon={faLinkedin} className="me-3 footer-icons"/></a>
+      <a href="https://api.whatsapp.com/send/?phone=%2B524422052403&text&app_absent=0" target="_blank"><FontAwesomeIcon icon={faWhatsapp} className="me-3 footer-icons"/></a>
+      <a href="mailto:ventasleder@hotmail.com" target="_blank"><FontAwesomeIcon icon={faEnvelope} className=" footer-icons"/></a>
     </div>
   );
 }
