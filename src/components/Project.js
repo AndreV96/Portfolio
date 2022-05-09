6ff30fd0ad52c4c93499ca58cb909a2f7ff3313e
André Vega <ventaslederer@hotmail.com>
2022-05-09 17:35:49 -0500

    Fixed images source

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,7 +9,7 @@ function Project() {
     <h1 className="mt-3">Projects</h1>
       <Container className="my-3 projects-container align-items-center justify-content-center">
         <a href="https://limitless-plateau-15055.herokuapp.com/" target="_blank" className="whistle-card"><Card className="bg-dark text-white d-block project-card">
-          <Card.Img src="Whistle2.png" alt="Card image" className="project-img"/>
+          <Card.Img src={process.env.PUBLIC_URL + "/Whistle2.png"} alt="Card image" className="project-img"/>
           <Card.ImgOverlay className="text-start card-overlay">
             <Card.Title className="d-inline-block">Whistle</Card.Title>
             <a href="https://github.com/AndreV96/Whistle" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block projects-github-icon"/></a> 
@@ -20,7 +20,7 @@ function Project() {
         </Card></a>
         
         <a href="https://andrev96.github.io/Moviez/" target="_blank"><Card className="bg-dark text-white d-block project-card">
-          <Card.Img src="moviez_individual.png" alt="Card image" className="project-img"/>
+          <Card.Img src={process.env.PUBLIC_URL + "/moviez_individual.png"} alt="Card image" className="project-img"/>
           <Card.ImgOverlay className="text-start card-overlay">
             <Card.Title className="d-inline-block">Moviez</Card.Title>
             <a href="https://github.com/AndreV96/Moviez/" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block projects-github-icon"/></a> 
@@ -31,7 +31,7 @@ function Project() {
         </Card></a>
 
         <a href="https://morning-lowlands-78728.herokuapp.com/" target="_blank"><Card className="bg-dark text-white d-block project-card">
-          <Card.Img src="Note_Taker.jpg" alt="Card image" className="project-img"/>
+          <Card.Img src={process.env.PUBLIC_URL + "/Note_Taker.jpg"} alt="Card image" className="project-img"/>
           <Card.ImgOverlay className="text-start card-overlay">
             <Card.Title className="d-inline-block">Simple Note</Card.Title>
             <a href="https://github.com/AndreV96/Simple_Note" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block projects-github-icon"/></a> 
@@ -42,7 +42,7 @@ function Project() {
         </Card></a>
 
         <a href="https://andrev96.github.io/Forecasting/" target="_blank"><Card className="bg-dark text-white d-block project-card">
-          <Card.Img src="weather_dashboard.png" alt="Card image" className="project-img"/>
+          <Card.Img src={process.env.PUBLIC_URL + "/weather_dashboard.png"} alt="Card image" className="project-img"/>
           <Card.ImgOverlay className="text-start card-overlay">
             <Card.Title className="d-inline-block">Forecasting</Card.Title>
             <a href="https://github.com/AndreV96/Forecasting/" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block projects-github-icon"/></a> 
@@ -53,7 +53,7 @@ function Project() {
         </Card></a>
 
         <a href="https://andrev96.github.io/Every_Day/" target="_blank" className="ever-day-card"><Card className="bg-dark text-white d-block project-card ">
-          <Card.Img src="work_day.jpg" alt="Card image" className="project-img every-day-img"/>
+          <Card.Img src={process.env.PUBLIC_URL + "/work_day.jpg"} alt="Card image" className="project-img every-day-img"/>
           <Card.ImgOverlay className="text-start card-overlay">
             <Card.Title className="d-inline-block">Every Day</Card.Title>
             <a href="https://github.com/AndreV96/Every_Day/" target="_blank"><FontAwesomeIcon icon={faGithub}  className="ms-2 d-inline-block projects-github-icon"/></a> 
